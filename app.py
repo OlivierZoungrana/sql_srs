ac46054a303ec7f2e167949da8c6d0711db017d4
synthejazz <synthejazz@gmail.com>
2025-04-20 13:28:48 +0200

    add thme select

--- app.py
+++ app.py
@@ -2,11 +2,25 @@ import streamlit as st
 import pandas as pd
 import duckdb as db
 
+st.write("""
+#SQL SRS 
+SPACE REPETITION system SQL PRACTICE
+"""
+     )
+
 data= {"a": [1, 2, 3], "b": [4, 5, 6]}
 
 df= pd.DataFrame(data)
 tab1,tab2,tab3 = st.tabs(["cat", "dog", "owm"])
 
+option = st.selectbox(
+    "What would you like to review?",
+    ("Join", "GroupBy", "Windows function"),
+    index=None,
+    placeholder="select theme..."
+)
+
+st.write('you selected : ', option)
 with tab1:
     input_text=st.text_area(label="entrer votre input:")
     result =db.sql(input_text)
