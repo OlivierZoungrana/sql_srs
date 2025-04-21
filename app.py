d52bcaafdadd7f1e1cc225d892a64b87112322c1
OlivierZoungrana <66361271+OlivierZoungrana@users.noreply.github.com>
2025-04-21 10:58:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,28 +2,34 @@ import streamlit as st
 import pandas as pd
 import duckdb as db
 
-data= {"a": [1, 2, 3], "b": [4, 5, 6]}
+st.write("""
+#SQL SRS 
+SPACE REPETITION system SQL PRACTICE
+"""
+         )
 
+data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 
 st.write("""
 SQL REVISION SITE
 """)
-
-option=st.selectbox(
-    "what would like to revise ?",
-    ("join", "GroupBy", "Windows function"),
-    index=None,
-    placeholder="select theme ..."
+with st.sidebar:
+    option = st.selectbox(
+        "what would like to revise ?",
+        ("join", "GroupBy", "Windows function"),
+        index=None,
+        placeholder="select theme ..."
 )
 
 st.write('you selected : ', option)
-df= pd.DataFrame(data)
-tab1,tab2,tab3 = st.tabs(["cat", "dog", "owm"])
+df = pd.DataFrame(data)
+tab1, tab2, tab3 = st.tabs(["cat", "dog", "owm"])
 
+
+st.write('you selected : ', option)
 with tab1:
-    input_text=st.text_area(label="entrer votre input:")
-    result =db.sql(input_text)
+    input_text = st.text_area(label="entrer votre input:")
+    result = db.sql(input_text)
     st.write(result)
 
     st.write(input_text)
-    st.dataframe(df)
